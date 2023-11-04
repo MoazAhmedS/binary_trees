12d597138ec0470b2af03c039ce3ce026c83d05d
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2023-11-03 19:25:09 -0700

    task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - new node
+ * binary_tree_node - Create a binary tree node.
  *
- * @parent: point to the node
- * @value: value
+ * @parent: A pointer to the parent of the node.
+ * @value: The value to be puted on the new node.
  *
- * Return: result
+ * Return: If it fails - NULL else - a pointer to the new node.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
